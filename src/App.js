496672202d39ad7b47c68003cca363f62557cdb4
Mounika Rudra <mounika.rudra2010@gmail.com>
2025-01-13 23:56:13 -0600

    added get started content

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header";
+import GetStartedSection from "./components/GetStartedSection";
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <GetStartedSection></GetStartedSection>
     </div>
   );
 }
